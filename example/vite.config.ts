8ec696098ea89ba5487e2b8983c40a2558e66545
Marin Heđeš <hedesmarin@gmail.com>
2026-03-20 22:35:10 +0100

    feat(deploy): setup gh action

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-fit-list/",
   resolve: {
     alias: {
       "react-fit-list": fileURLToPath(
